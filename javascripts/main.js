22ec071fa30660749deebb697fd6f7db2a4aacbb
jkcchan <jkcchan@uwaterloo.ca>
2015-02-07 12:55:23 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 
 	$( ".header" ).delay( 200 ).slideDown(1200);
 	$(".heading2").fadeIn(1200);
-	$( ".summary" ).delay( 400 ).fadeIn( 2000 );
-	$( ".experience" ).delay( 600 ).fadeIn( 1600 );
-	$( ".relevantAssignments" ).delay( 800 ).fadeIn( 1200 );
-	$( ".education" ).delay( 1000 ).fadeIn( 800 );
-	$( ".awardsandinterests" ).delay( 1200 ).fadeIn( 400 );
-	$(".footer").delay(1400).fadeIn(400);
+	$( ".summary" ).fadeIn( 2000 );
+	$( ".experience" ).fadeIn( 1600 );
+	$( ".relevantAssignments" ).fadeIn( 1200 );
+	$( ".education" ).fadeIn( 800 );
+	$( ".awardsandinterests" ).fadeIn( 400 );
+	$(".footer").fadeIn(400);
 	$(".footer").mouseenter(function(){
 		$(".footer>h3").text("Contact me!");
 	});
