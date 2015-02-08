6595beaed84a462cf4c075da624bdc633d0c2492
jkcchan <jkcchan@uwaterloo.ca>
2015-02-07 23:07:27 -0500

    Update main.js
    big change

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,68 @@
+function changeActive(){
+		$(".visible").fadeOut(0);
+		$(".visible").removeClass("visible");
+		$(".active").removeClass("active");
+};
+function resumeFade(){
+		$( ".header" ).delay( 200 ).slideDown(1200);
+		$(".heading2").fadeIn(1200);
+		$( ".summary" ).fadeIn( 2000 );
+		$( ".experience" ).fadeIn( 1600 );
+		$( ".relevantAssignments" ).fadeIn( 1200 );
+		$( ".education" ).fadeIn( 800 );
+		$( ".awardsandinterests" ).fadeIn( 400 );
+		$(".footer").fadeIn(400);
+}
 $(document).ready(function(){
+	$(".home").addClass("visible");
+	$(".home").fadeIn("slow");
+	$(".resume-button-alt").click(function(){
+		$(".resume-button").click();
+	});
+	$(".skills-button-alt").click(function(){
+		$(".skills-button").click();
+	});
+	$(".projects-button-alt").click(function(){
+		$(".projects-button").click();
+	})
+	$(".resume-button").click(function(){
+		changeActive();
+		$(this).addClass("active");
+		$(".resume").addClass("visible");
+		$(".resume").fadeIn("fast");
+		resumeFade();
+		$(".footer").mouseenter(function(){
+			$(".footer>h3").text("Contact me!");
+		});
 
-	$( ".header" ).delay( 200 ).slideDown(1200);
-	$(".heading2").fadeIn(1200);
-	$( ".summary" ).fadeIn( 2000 );
-	$( ".experience" ).fadeIn( 1600 );
-	$( ".relevantAssignments" ).fadeIn( 1200 );
-	$( ".education" ).fadeIn( 800 );
-	$( ".awardsandinterests" ).fadeIn( 400 );
-	$(".footer").fadeIn(400);
-	$(".footer").mouseenter(function(){
-		$(".footer>h3").text("Contact me!");
 	});
-	$(".footer").mouseleave(function(){
-		$(".footer>h3").text("Copyright Jacob Chan 2015(c)");
+	$(".skills-button").click(function(){
+		changeActive();
+		$(this).addClass("active");
+		$(".skills").addClass("visible");
+		$(".skills").fadeIn("slow");
 	});
-	$(".header").mouseenter(function(){
-		$(".header>h1").append("<span>!</span>");
+	$(".navbar-brand").click(function(){
+		changeActive();
+		$(".home").addClass("visible");
+		$(".home").fadeIn("fast");
 	});
-
+	$(".projects-button").click(function(){
+		changeActive();
+		$(this).addClass("active");
+		$(".projects").addClass("visible");
+		$(".projects").fadeIn("slow");
+	});
+	$(".misc-button").click(function(){
+		changeActive();
+		$(this).addClass("active");
+		$(".misc").addClass("visible");
+		$(".misc").fadeIn("slow");
+	});
+	$(".contact-button").click(function(){
+		changeActive();
+		$(this).addClass("active");
+		$(".contact").addClass("visible");
+		$(".contact").fadeIn("slow");
+	});
 });
